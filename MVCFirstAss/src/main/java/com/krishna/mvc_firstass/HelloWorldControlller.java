a24592aee2667d64858262f9c08d077a1136e02d
BaikaniKrishna <93122574+BaikaniKrishna@users.noreply.github.com>
2021-11-27 20:32:48 +0530

    Add files via upload

---
+++ MVCFirstAss/src/main/java/com/krishna/mvc_firstass/HelloWorldControlller.java
@@ -0,0 +1,16 @@
+package com.krishna.mvc_firstass;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldControlller {
+ @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+    public String index(ModelMap model){
+	 System.out.println("Index method working");
+        model.addAttribute("message", "cargaGeneracion");
+        return "helloworld";
+    }
+}
---
+++ MVC_Third_Assignment/src/main/java/com/krishna/thirtassignment/UserController.java
@@ -0,0 +1,36 @@
+package com.krishna.thirtassignment;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/loginShow")
+	public String showLogin()
+	{
+		return "login";
+	}
+	
+	@RequestMapping(path="/checkingEntry",method=RequestMethod.POST)
+	public String receiveInfo(@ModelAttribute User user)
+	{
+		if (true) 
+			
+		
+		{
+			
+		
+		
+			return "error";
+		}
+		else
+		{
+			return "success";
+		}
+	}
+}
